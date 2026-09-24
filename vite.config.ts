b42b5fefa7f9e8ab87dcaa5bd4603d31a804ad12
Rupal2230 <rupalwankhade2230@gmail.com>
2026-09-24 22:26:22 +0530

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/Student-Navigator/',
+    base: '/Student-Navigator-/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
